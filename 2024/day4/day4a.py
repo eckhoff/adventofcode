797e11e1d31dd705fff48deee025cc13ead21dec
Andrew Eckhoff <eckhoff@protonmail.com>
2024-12-11 17:36:23 -0600

    finshed day 4 part 1

--- 2024/day4/day4a.py
+++ 2024/day4/day4a.py
@@ -1,25 +1,13 @@
 xmas_matrix = []
-with open('test.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     for line in file:
         stripped_line = line.strip()
         row = list(stripped_line)
         xmas_matrix.append(row)
-'''
-for i in xmas_matrix:
-    for j in range(len(i)):
-        if i[j] == 'X':
-            try:
-                if i[j+1] == 'M' and i[j+2] == 'A' and i[j+3] == 'S':
-                    print(i[j] + i[j+1] + i[j+2] + i[j+3])
-            except IndexError:
-                pass
-            try:
-                if i[j-1] == 'M' and i[j-2] == 'A' and i[j-3] == 'S':
-                    print(i[j] + i[j-1] + i[j-2] + i[j-3])
-            except IndexError:
-                pass
-'''
+
+
 result = 0
+
 for i in range(len(xmas_matrix)):
     for j in range(len(xmas_matrix[i])):
         if xmas_matrix[i][j] == 'X':
@@ -29,8 +17,9 @@ for i in range(len(xmas_matrix)):
             except IndexError:
                 pass
             try:
-                if xmas_matrix[i][j-1] == 'M' and xmas_matrix[i][j-2] == 'A' and xmas_matrix[i][j-3] == 'S':
-                    result += 1
+                if j-1 >=0 and j-2 >=0 and j-3 >=0:
+                    if xmas_matrix[i][j-1] == 'M' and xmas_matrix[i][j-2] == 'A' and xmas_matrix[i][j-3] == 'S':
+                        result += 1
             except IndexError:
                 pass
             try:
@@ -39,8 +28,9 @@ for i in range(len(xmas_matrix)):
             except IndexError:
                 pass
             try:
-                if xmas_matrix[i-1][j] == 'M' and xmas_matrix[i-2][j] == 'A' and xmas_matrix[i-3][j] == 'S':
-                    result += 1
+                if i-1 >=0 and i-2 >=0 and i-3 >=0:
+                    if xmas_matrix[i-1][j] == 'M' and xmas_matrix[i-2][j] == 'A' and xmas_matrix[i-3][j] == 'S':
+                        result += 1
             except IndexError:
                 pass
             try:
@@ -49,20 +39,22 @@ for i in range(len(xmas_matrix)):
             except IndexError:
                 pass
             try:
-                if xmas_matrix[i-1][j-1] == 'M' and xmas_matrix[i-2][j-2] == 'A' and xmas_matrix[i-3][j-3] == 'S':
-                    result += 1
+                if i-1 >=0 and j-1 >=0 and i-2 >=0 and j-2 >=0 and i-3 >=0 and j-3 >=0:
+                    if xmas_matrix[i-1][j-1] == 'M' and xmas_matrix[i-2][j-2] == 'A' and xmas_matrix[i-3][j-3] == 'S':
+                        result += 1
             except IndexError:
                 pass
             try:
-                if xmas_matrix[i+1][j-1] == 'M' and xmas_matrix[i+2][j-2] == 'A' and xmas_matrix[i+3][j-3] == 'S':
-                    result += 1
+                if j-1 >=0 and j-2 >=0 and j-3 >=0:
+                    if xmas_matrix[i+1][j-1] == 'M' and xmas_matrix[i+2][j-2] == 'A' and xmas_matrix[i+3][j-3] == 'S':
+                        result += 1
             except IndexError:
                 pass
             try:
-                if xmas_matrix[i-1][j+1] == 'M' and xmas_matrix[i-2][j+2] == 'A' and xmas_matrix[i-3][j+3] == 'S':
-                    result += 1
+                if i-1 >=0 and i-2 >=0 and i-3 >=0:
+                    if xmas_matrix[i-1][j+1] == 'M' and xmas_matrix[i-2][j+2] == 'A' and xmas_matrix[i-3][j+3] == 'S':
+                        result += 1
             except IndexError:
                 pass
 
-print(result)
-            
+print(result) 
